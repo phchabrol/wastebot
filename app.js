bc0c8deacfa0df721fceaac66d39b19f1b7750f6
phchabrol <paulhenri.chabrol@gmail.com>
2018-03-18 22:55:47 +0100

    upload images works

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var restify = require('restify');
 var builder = require('botbuilder');
+var needle = require('needle');
 
 require('dotenv').config();
 
@@ -25,49 +26,34 @@ var bot = new builder.UniversalBot(connector, [
     function (session) {
         session.beginDialog('greetings', session.dialogData.name);
     },
-    function (session, results) {
-        // Process request and display reservation details
-        session.send(`Well, let's get started %s, please send me a picture of your trash`, session.userData.name);
+    function (session,results) {
+        builder.Prompts.attachment(session, `Well, let's get started, please send me a picture of your trash`);
     },
-    function (session, results) {
-        var msg = session.message;
-        if (msg.attachments && msg.attachments.length > 0) {
-         // Echo back attachment
-         var attachment = msg.attachments[0];
-            session.send({
-                text: "You sent:",
-                attachments: [
-                    {
-                        contentType: attachment.contentType,
-                        contentUrl: attachment.contentUrl,
-                        name: attachment.name
-                    }
-                ]
-            });
-        } else {
-            // Echo back users text
-            session.send("You said: %s", session.message.text);
+    function (session,results) {
+        console.log('attachement requested');
+        if (hasImageAttachment(session)) {
+            var stream = getImageStreamFromMessage(session.message);
+            session.send("got the image in the attachment");
+          } else {
+            var imageUrl = parseAnchorTag(session.message.text) || (validUrl.isUri(session.message.text) ? session.message.text : null);
+            if (imageUrl) {
+                session.send("got the image in the URL");
+              } else {
+                session.send('Did you upload an image? I\'m more of a visual person. Try sending me an image or an image URL');
+            }
         }
-        session.endDialog("Thanks, see you later");
     }
+
 ]).set('storage', inMemoryStorage); // Register in-memory storage 
 
 
 bot.dialog('greetings', [
     function (session) {
-        session.beginDialog('askName');
+        builder.Prompts.text(session, 'Hi! What is your name?');
     },
     function (session, results) {
         session.userData.name = results.response;
         session.endDialog('Hello %s!', results.response);
-    }
-]);
-bot.dialog('askName', [
-    function (session) {
-        builder.Prompts.text(session, 'Hi! What is your name?');
-    },
-    function (session, results) {
-        session.endDialogWithResult(results);
     }
 ]);
 
@@ -82,3 +68,48 @@ bot.dialog('help', function (session, args, next) {
         session.beginDialog(args.action, args);
     }
 });
+
+function hasImageAttachment(session) {
+    console.log("check attachment");
+    return session.message.attachments.length > 0 &&
+        session.message.attachments[0].contentType.indexOf('image') !== -1;
+}
+
+function getImageStreamFromMessage(message) {
+    var headers = {};
+    var attachment = message.attachments[0];
+    if (checkRequiresToken(message)) {
+        // The Skype attachment URLs are secured by JwtToken,
+        // you should set the JwtToken of your bot as the authorization header for the GET request your bot initiates to fetch the image.
+        // https://github.com/Microsoft/BotBuilder/issues/662
+        connector.getAccessToken(function (error, token) {
+            var tok = token;
+            headers['Authorization'] = 'Bearer ' + token;
+            headers['Content-Type'] = 'application/octet-stream';
+
+            return needle.get(attachment.contentUrl, { headers: headers });
+        });
+    }
+
+    headers['Content-Type'] = attachment.contentType;
+    return needle.get(attachment.contentUrl, { headers: headers });
+}
+
+function checkRequiresToken(message) {
+    return message.source === 'skype' || message.source === 'msteams';
+}
+
+/**
+ * Gets the href value in an anchor element.
+ * Skype transforms raw urls to html. Here we extract the href value from the url
+ * @param {string} input Anchor Tag
+ * @return {string} Url matched or null
+ */
+function parseAnchorTag(input) {
+    var match = input.match('^<a href=\"([^\"]*)\">[^<]*</a>$');
+    if (match && match[1]) {
+        return match[1];
+    }
+
+    return null;
+}

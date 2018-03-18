666a2daba671fa617aeb0631d4541147a714f478
phchabrol <paulhenri.chabrol@gmail.com>
2018-03-18 21:23:12 +0100

    adapted with question on greetings and name

--- app.js
+++ app.js
@@ -23,22 +23,13 @@ var inMemoryStorage = new builder.MemoryBotStorage();
 // This is a dinner reservation bot that uses a waterfall technique to prompt users for input.
 var bot = new builder.UniversalBot(connector, [
     function (session) {
-        session.send("Welcome to the dinner reservation.");
-        builder.Prompts.time(session, "Please provide a reservation date and time (e.g.: June 6th at 5pm)");
+        session.send("Welcome to the reduce waste bot.");
+        builder.Prompts.text(session, "First tell me your name");
     },
     function (session, results) {
-        session.dialogData.reservationDate = builder.EntityRecognizer.resolveTime([results.response]);
-        builder.Prompts.text(session, "How many people are in your party?");
-    },
-    function (session, results) {
-        session.dialogData.partySize = results.response;
-        builder.Prompts.text(session, "Whose name will this reservation be under?");
-    },
-    function (session, results) {
-        session.dialogData.reservationName = results.response;
-
+        session.dialogData.name = results.response;
         // Process request and display reservation details
-        session.send(`Reservation confirmed. Reservation details: <br/>Date/Time: ${session.dialogData.reservationDate} <br/>Party size: ${session.dialogData.partySize} <br/>Reservation name: ${session.dialogData.reservationName}`);
+        session.send(`Understood,  ${session.dialogData.name}, let's get started!`);
         session.endDialog();
     }
 ]).set('storage', inMemoryStorage); // Register in-memory storage 
